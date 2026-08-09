f7e2f5771a050005495b516dd0ed1b7e5ee62ade
Phani Harsha Nuggu <phaniharsharam@gmail.com>
2026-08-09 21:48:58 +0530

    added transaction router

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.use('/api/transactions', transactionRoutes);
 app.listen(3001,() => {
     console.log("Node server started");
 });
