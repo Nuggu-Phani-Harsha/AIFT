1175525083f6b077de2c8fe8dbaeb31bbbfd7228
Phani Harsha Nuggu <phaniharsharam@gmail.com>
2026-08-11 22:37:13 +0530

    adding mongodb connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,21 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+import transactionRoutes from './routes/transaction.routes.js';
 
 app.use('/api/transactions', transactionRoutes);
 app.listen(3001,() => {
     console.log("Node server started");
 });
+
+ mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('Connected to MongoDB');
+    app.listen(PORT, () => {
+      console.log(` Server running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error(' MongoDB connection error:', error);
+    process.exit(1);
+  });
