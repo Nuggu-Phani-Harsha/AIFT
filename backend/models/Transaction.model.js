dbf9763b138bdbce864b8b9eb6439141533c383e
Phani Harsha Nuggu <phaniharsharam@gmail.com>
2026-08-14 21:54:47 +0530

    temporary testing

--- backend/models/Transaction.model.js
+++ backend/models/Transaction.model.js
@@ -1,12 +1,12 @@
 import mongoose from 'mongoose';
 
 const transactionSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-    index: true
-  },
+  // user: {
+  //   type: mongoose.Schema.Types.ObjectId,
+  //   ref: 'User',
+  //   required: true,
+  //   index: true
+  // },
   type: {
     type: String,
     enum: ['income', 'expense'],
