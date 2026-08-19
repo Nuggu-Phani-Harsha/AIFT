16e2b39b7760411ed408b4aed4607cd2a758207a
Phani Harsha Nuggu <phaniharsharam@gmail.com>
2026-08-19 19:59:34 +0530

    some formatting

--- backend/server.js
+++ backend/server.js
@@ -2,19 +2,21 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 
-const app = express();
+
 import transactionRoutes from './routes/transaction.routes.js';
 import authRoutes from './routes/auth.routes.js';
+
 dotenv.config();
+
+const app = express();
 const PORT = process.env.PORT || 3001;
+
 app.use(express.json());
+
 app.use('/api/transactions', transactionRoutes);
 app.use('/api/auth',authRoutes);
-app.listen(3001,() => {
-    console.log("Node server started");
-});
 
- mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log('Connected to MongoDB');
     app.listen(PORT, () => {
@@ -25,3 +27,7 @@ app.listen(3001,() => {
     console.error(' MongoDB connection error:', error);
     process.exit(1);
   });
+
+app.listen(3001,() => {
+    console.log("Node server started");
+});
